9d4f8f4b9f78617305946bcc53c4022f310e3465
Shivakumar M <shivakumar12054@gmail.com>
2024-12-31 10:50:40 +0530

    chore(how-it-works): move components to page, add all images to image kit

--- src/app/(public_routes)/how-it-works/page.tsx
+++ src/app/(public_routes)/how-it-works/page.tsx
@@ -1,8 +1,19 @@
 "use client";
 
+import FirstBoxDetails from "@/components/how-it-works/FirstBoxSection";
+import Header from "@/components/how-it-works/Header";
+import NutritionSection from "@/components/how-it-works/NutritionSection";
+import StepSection from "@/components/how-it-works/StepSection";
+import SubscriptionSection from "@/components/how-it-works/Subscribption";
+import Faq from "../faq/page";
 
 export default function HowItWorksPage() {
-  <div className="App">
-
-    </div>
+  <div>
+    <Header />
+    <StepSection />
+    <NutritionSection />
+    <FirstBoxDetails />
+    <SubscriptionSection />
+    <Faq />
+  </div>;
 }
