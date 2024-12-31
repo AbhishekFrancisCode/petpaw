50d472649c100848743e61af617e6c9205e0043c
Shivakumar M <shivakumar12054@gmail.com>
2024-12-31 10:50:40 +0530

    chore(how-it-works): move components to page, add all images to image kit

--- src/app/(public_routes)/how-it-works/layout.tsx
+++ src/app/(public_routes)/how-it-works/layout.tsx
@@ -1,26 +1,17 @@
 "use client";
-import FirstBoxDetails from "@/components/how-it-works/FirstBoxSection";
-import Header from "@/components/how-it-works/Header";
+// import FirstBoxDetails from "@/components/how-it-works/FirstBoxSection";
+// import Header from "@/components/how-it-works/Header";
 
-import NutritionSection from "@/components/how-it-works/NutritionSection";
+// import NutritionSection from "@/components/how-it-works/NutritionSection";
 
-import StepSection from "@/components/how-it-works/StepSection";
-import SubscriptionSection from "@/components/how-it-works/Subscribption";
-import Faq from "@/components/how-it-works/Faq";
+// import StepSection from "@/components/how-it-works/StepSection";
+// import SubscriptionSection from "@/components/how-it-works/Subscribption";
+// import Faq from "@/components/how-it-works/Faq";
 
-
-
-
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function HowItWorksLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col mx-auto min-h-screen">
-      <Header />
-      <StepSection />
-      <NutritionSection />
-      <FirstBoxDetails />
-      <SubscriptionSection />
-      <Faq />
+    <div className="min-h-screen mx-auto bg-[#FFF8EF]">
+      <main className="">{children}</main>
     </div>
   );
 }
