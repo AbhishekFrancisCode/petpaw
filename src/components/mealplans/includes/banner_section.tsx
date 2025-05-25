76c6d8d30f287bf90dc2d86f84a85dac358644df
Abhishek Francis <abhi09shek@gmail.com>
2025-05-25 11:59:30 +0530

    fix mobile screens issues

--- src/components/mealplans/includes/banner_section.tsx
+++ src/components/mealplans/includes/banner_section.tsx
@@ -1,7 +1,5 @@
 "use client";
-import Button from "@/sb-ui/button/button";
 import Image from "next/image";
-import DogMealPlan from "../../product-detail/includes/banner-2-grid";
 
 export default function BannerSection() {
   const mealOption = [
@@ -14,38 +12,40 @@ export default function BannerSection() {
       recipes: ["Chicken Dish with Carrots", "Lamb Dish with Cranberries", "Pork Dish with Apples"]
     }
   ];
+
   return (
     <section
-      className="place-content-center py-4 px-64 max-w-[98%] mx-auto rounded-xl"
+      className="w-full mx-auto py-6 px-4 sm:px-8 lg:px-16 xl:px-24 rounded-xl"
       style={{
-        // backgroundImage: `url(${url})`,
         backgroundColor: "#C4DEE1",
         backgroundPosition: "right bottom",
         backgroundRepeat: "no-repeat"
       }}
     >
-      <div className="flex flex-col md:flex-row gap-6">
-        <div className="md:w-1/2 flex flex-col min-h-full bg-transparent place-content-center rounded-lg p-6 gap-6">
-          <div className="flex items-center gap-2 mb-4">
-            <h2 className="text-xl font-semibold">{mealOption[0].name}</h2>
+      <div className="flex flex-col md:flex-row gap-6 items-center">
+        {/* Text Column */}
+        <div className="md:w-1/2 flex flex-col justify-center bg-transparent rounded-lg p-4 sm:p-6 gap-4">
+          <div className="flex items-center gap-2 mb-2">
+            <h2 className="text-2xl font-semibold">{mealOption[0].name}</h2>
             <span className="bg-black text-white text-xs px-2 py-1 rounded">
               {mealOption[0].tag}
             </span>
           </div>
 
           <p className="text-lg font-medium">{mealOption[0].price}</p>
-          <p className="mt-2 mb-6">{mealOption[0].description}</p>
+          <p className="mt-2 mb-6 text-sm sm:text-base">{mealOption[0].description}</p>
 
-          <button className="w-[200px] bg-[#FFF8EF] text-[#333333] py-3 px-6 rounded-md hover:bg-[#028391] hover:text-white  transition-colors">
+          <button className="w-full sm:w-[200px] bg-[#FFF8EF] text-[#333333] py-3 px-6 rounded-md hover:bg-[#028391] hover:text-white transition-colors">
             Get your treats
           </button>
         </div>
 
-        <div className="md:w-1/2 bg-transparent rounded-lg overflow-hidden">
+        {/* Image Column */}
+        <div className="md:w-1/2 bg-transparent rounded-lg overflow-hidden max-w-full">
           <img
             src="https://ik.imagekit.io/funlogic/pawfull/all_snacks-cut%20out%20.png?updatedAt=1745739800517"
             alt="Dog eating from bowl"
-            className="w-full h-full max-h-[550px] object-cover"
+            className="w-full max-h-[400px] md:max-h-[500px] object-contain"
           />
         </div>
       </div>
