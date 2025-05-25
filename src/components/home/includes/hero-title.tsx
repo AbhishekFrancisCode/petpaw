14c235f61fe14fed9b14199aee9ff5a11561e4bc
Abhishek Francis <abhi09shek@gmail.com>
2025-05-25 12:42:04 +0530

    fix topology

--- src/components/home/includes/hero-title.tsx
+++ src/components/home/includes/hero-title.tsx
@@ -1,6 +1,11 @@
 import { HomePageStrings } from "@/constants/title-constants";
 
-export default function HeroTitle() {
+interface HeroTitleProps {
+  title?: string;
+  description?: string;
+}
+
+export default function HeroTitle({ title, description }: HeroTitleProps) {
   return (
     <div className="flex w-full h-full justify-center lg:justify-start items-start text-center lg:text-left px-4 sm:px-6 lg:px-0">
       <div className="flex flex-col max-w-4xl">
@@ -11,7 +16,7 @@ export default function HeroTitle() {
             lineHeight: "1.2"
           }}
         >
-          Freshly Cooked,
+          {title || ""}
         </p>
         <p
           className="text-[#EE9422] font-semibold py-1"
@@ -20,7 +25,7 @@ export default function HeroTitle() {
             lineHeight: "1.2"
           }}
         >
-          Meals for Dogs!
+          {description || ""}
         </p>
         <p className="text-base md:text-lg font-medium mt-6 lg:pr-32">
           {HomePageStrings.description}
